bd19f182390410e716a56255802196aed6bd335e
JacobKorin <jacob.korin@ontariotechu.net>
2023-11-29 14:50:14 -0500

    more auth. updates

--- config/app.js
+++ config/app.js
@@ -67,8 +67,6 @@ let workoutsRouter = require('../routes/workouts');
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/workoutslist',workoutsRouter);
-app.use('/login',loginRouter);
-app.use('/register', registerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
